7d3a524913cdb164ce8b2bd01587e7cde1f88874
haseo <haseo@192.168.219.137>
2018-01-17 19:56:43 +0900

    쫑

--- DatePlanner/src/hello.java
+++ DatePlanner/src/hello.java
@@ -1,9 +1,13 @@
 public class hello {
+	
 	public static void main(String[] args) {
+		
 		System.out.println("유선일 커밋테스트");
 		System.out.println("박세진 커밋");
 		System.out.println("윤록귀 안녕");
 		System.out.println("선일이 안되냐");
 		System.out.println("성종이 안되냐");
+		
 	}
+	
 }
