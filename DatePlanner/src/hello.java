4cb3139c6a55aba869aa17bef06109fb25500b56
dbtjsdlf222 <34783191+dbtjsdlf222@users.noreply.github.com>
2018-01-17 20:25:39 +0900

    Signed-off-by: dbtjsdlf222 <34783191+dbtjsdlf222@users.noreply.github.com>

--- DatePlanner/src/hello.java
+++ DatePlanner/src/hello.java
@@ -1,7 +1,5 @@
 public class hello {
-
 	public static void main(String[] args) {
-
 		System.out.println("유선일 커밋테스트");
 		System.out.println("박세진 커밋");
 		System.out.println("윤록귀 안녕");
@@ -9,7 +7,5 @@ public class hello {
 		System.out.println("성종이 안되냐");
 		System.out.println("록기록기");
 		System.out.println("록기록기");
-
 	}
-
 }
