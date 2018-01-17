d6abc4c0e47b342375e09d1d482909ff88dd9b04
haseo <haseo@192.168.219.137>
2018-01-17 21:39:44 +0900

    asdf

---
+++ DatePlanner/src/org/dateplanner/api/FacebookLoginAPI.java
@@ -0,0 +1,14 @@
+package org.dateplanner.api;
+
+import org.dateplanner.vo.LoginInfo;
+
+public class FacebookLoginAPI implements LoginAPI {
+	
+	@Override
+	public LoginInfo login(String arg) {
+		
+		return null;
+		
+	} //login();
+	
+} //class FacebookLoginAPI;
---
+++ DatePlanner/src/org/dateplanner/api/LoginAPI.java
@@ -0,0 +1,9 @@
+package org.dateplanner.api;
+
+import org.dateplanner.vo.LoginInfo;
+
+public interface LoginAPI {
+	
+	public LoginInfo login(String arg);
+	
+} //interface LoginAPI;
