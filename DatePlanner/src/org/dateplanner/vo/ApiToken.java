0a952206d7e96b9507aeeac796d6b0702f9932b2
haseo <haseo@192.168.219.137>
2018-01-18 19:29:53 +0900

    asdasd

--- DatePlanner/src/org/dateplanner/vo/ApiToken.java
+++ DatePlanner/src/org/dateplanner/vo/ApiToken.java
@@ -3,11 +3,14 @@ package org.dateplanner.vo;
 public class ApiToken {
 	
 	//platform: D = DatePlanner, N = Naver, F = Facebook
+	private int no;
 	private char platform;
 	private String key;
 	
+	public int getNo() { return no; }
 	public char getPlatform() { return platform; }
 	public String getKey() { return key; }
+	public void setNo(int no) { this.no = no; }
 	public void setPlatform(char platform) { this.platform = platform; }
 	public void setKey(String key) { this.key = key; }
 	
