911be403dc649a90500e4da81a665e27ea28d40a
윤록귀 <@DESKTOP-SUU5012>
2018-01-23 21:37:12 +0900

    asdf

--- DatePlanner/src/org/dateplanner/googletemp/googleVo.java
+++ DatePlanner/src/org/dateplanner/googletemp/googleVo.java
@@ -1,15 +1,41 @@
 package org.dateplanner.googletemp;
 
+import org.dateplanner.vo.LoginInfo;
+import org.springframework.social.google.connect.GoogleConnectionFactory;
+import org.springframework.social.oauth2.OAuth2Parameters;
+import org.springframework.stereotype.Controller;
+
+@Controller
 class googleVo {
 	
-	private String name;
-
-	public String getName() {
-		return name;
+	private LoginInfo user;
+	private GoogleConnectionFactory googleConnectionFactory;
+	private OAuth2Parameters googleOAuth2Parameters;
+	
+	public GoogleConnectionFactory getGoogleConnectionFactory() {
+		return googleConnectionFactory;
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	public void setGoogleConnectionFactory(GoogleConnectionFactory googleConnectionFactory) {
+		this.googleConnectionFactory = googleConnectionFactory;
 	}
+
+	public OAuth2Parameters getGoogleOAuth2Parameters() {
+		return googleOAuth2Parameters;
+	}
+
+	public void setGoogleOAuth2Parameters(OAuth2Parameters googleOAuth2Parameters) {
+		this.googleOAuth2Parameters = googleOAuth2Parameters;
+	}
+
+	public LoginInfo getUser() {
+		return user;
+	}
+
+	public void setUser(LoginInfo user) {
+		this.user = user;
+	}
+
+
 	
 }
