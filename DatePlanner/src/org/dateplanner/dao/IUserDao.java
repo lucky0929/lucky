ef542c34bd4548e437e8059970c9ca4202516413
dbtjsdlf222 <34783191+dbtjsdlf222@users.noreply.github.com>
2018-02-10 17:05:04 +0900

    userMapper

--- DatePlanner/src/org/dateplanner/dao/IUserDao.java
+++ DatePlanner/src/org/dateplanner/dao/IUserDao.java
@@ -6,5 +6,9 @@ import java.util.List;
 public interface IUserDao {
 	
 	public HashMap<String, Object> selectOne(String userId);
+	public void deleteOne (String userId);
+	public void insertUser(String userId);
+	public void selectNoWithId(String userId);
+	public void insertUserFacebook(String userId);
 	
 }
