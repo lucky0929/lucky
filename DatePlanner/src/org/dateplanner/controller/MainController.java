8495092fad18459b0be41baeff420c11a70c71f3
haseo <haseo@192.168.219.137>
2018-02-20 20:53:40 +0900

    asdf

--- DatePlanner/src/org/dateplanner/controller/MainController.java
+++ DatePlanner/src/org/dateplanner/controller/MainController.java
@@ -1,24 +1,13 @@
 package org.dateplanner.controller;
 
-import org.dateplanner.service.BoardService;
-import org.dateplanner.service.UsersService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MainController {
 	
-	@Autowired
-	private BoardService boardService;
-	
-	
 	@RequestMapping(path= { "/", "main", "index" })
 	public String main() {
-		
-		
-		
 		
 		return "main";
 		
