2de98170f1b0b838f5af2446bc7ced8fbe91c3a0
haseo <haseo@192.168.219.137>
2018-02-20 19:30:50 +0900

    asdf

---
+++ DatePlanner/src/org/dateplanner/controller/MainController.java
@@ -0,0 +1,16 @@
+package org.dateplanner.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+	
+	@RequestMapping(path= { "/", "main", "index" })
+	public String main() {
+		
+		return null;
+		
+	} //main();
+	
+} //class MainController;
