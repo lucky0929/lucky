1aa4160be3afd2bf27e75a4f31b118ae1ce21711
haseo <haseo@192.168.219.137>
2018-02-21 19:00:57 +0900

    asdf

--- DatePlanner/src/org/dateplanner/vo/Post.java
+++ DatePlanner/src/org/dateplanner/vo/Post.java
@@ -13,62 +13,27 @@ public class Post {
 	private String main_photo;
 	private char subject;
 	
-	
-	public int getNo() {
-		return no;
-	}
-	public void setNo(int no) {
-		this.no = no;
-	}
-	public int getRegion_no() {
-		return region_no;
-	}
-	public void setRegion_no(int region_no) {
-		this.region_no = region_no;
-	}
-	public int getUser_no() {
-		return user_no;
-	}
-	public void setUser_no(int user_no) {
-		this.user_no = user_no;
-	}
-	public int getHit() {
-		return hit;
-	}
-	public void setHit(int hit) {
-		this.hit = hit;
-	}
-	public Date getDate() {
-		return date;
-	}
-	public void setDate(Date date) {
-		this.date = date;
-	}
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public String getMain_photo() {
-		return main_photo;
-	}
-	public void setMain_photo(String main_photo) {
-		this.main_photo = main_photo;
-	}
-	public char getSubject() {
-		return subject;
-	}
-	public void setSubject(char subject) {
-		this.subject = subject;
-	}
+	public int getNo() {return no;}
+	public int getRegion_no() {return region_no;}
+	public int getUser_no() {return user_no;}
+	public int getHit() {return hit;}
+	public Date getDate() {return date;}
+	public String getTitle() {return title;}
+	public String getMain_photo() {return main_photo;}
+	public char getSubject() {return subject;}
+	public void setNo(int no) {this.no = no;}
+	public void setRegion_no(int region_no) {this.region_no = region_no;}
+	public void setUser_no(int user_no) {this.user_no = user_no;}
+	public void setHit(int hit) {this.hit = hit;}
+	public void setDate(Date date) {this.date = date;}
+	public void setTitle(String title) {this.title = title;}
+	public void setMain_photo(String main_photo) {this.main_photo = main_photo;}
+	public void setSubject(char subject) {this.subject = subject;}
 	
 	@Override
 	public String toString() {
 		return "Post [no=" + no + ", region_no=" + region_no + ", user_no=" + user_no + ", hit=" + hit + ", date="
 				+ date + ", title=" + title + ", main_photo=" + main_photo + ", subject=" + subject + "]";
-	}
-	
-	
+	} //toString();
 	
 } //class Post;
