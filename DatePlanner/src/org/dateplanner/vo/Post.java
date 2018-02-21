ccf93766f263da151eb3f4aa10f6ea79098ea38b
haseo <haseo@192.168.219.137>
2018-02-21 19:24:41 +0900

    asdf

--- DatePlanner/src/org/dateplanner/vo/Post.java
+++ DatePlanner/src/org/dateplanner/vo/Post.java
@@ -9,20 +9,20 @@ public class Post {
 	private String packageable;
 	
 	public int getNo() { return no; }
-	public void setNo(int no) { this.no = no; }
 	public String getMain_image() { return main_image; }
-	public void setMain_image(String main_image) { this.main_image = main_image; }
 	public String getTitle() { return title; }
-	public void setTitle(String title) { this.title = title; }
 	public String getContent() { return content; }
-	public void setContent(String content) { this.content = content; }
 	public String getPackageable() { return packageable; }
+	public void setNo(int no) { this.no = no; }
+	public void setMain_image(String main_image) { this.main_image = main_image; }
+	public void setTitle(String title) { this.title = title; }
+	public void setContent(String content) { this.content = content; }
 	public void setPackageable(String packageable) { this.packageable = packageable; }
-
+	
 	@Override
 	public String toString() {
 		return "Post [no=" + no + ", main_image=" + main_image + ", title=" + title + ", content=" + content
 				+ ", packageable=" + packageable + "]";
-	}
-		
+	} //toString();
+	
 } //class Post;
--- DatePlanner/src/org/dateplanner/vo/User.java
+++ DatePlanner/src/org/dateplanner/vo/User.java
@@ -4,39 +4,41 @@ package org.dateplanner.vo;
 import java.sql.Date;
 
 public class User {
-		private Integer no;
-	    private String name;
-	    private String key;
-	    private String nickname;
-	    private String sex;
-	    private String profile;
-	    private Integer regionNo;
-	    private String introduction;
-	    private Date regdate;	
-	    
-		public Integer getNo() { return no; }
-		public void setNo(Integer no) { this.no = no; }
-		public String getName() { return name; }
-		public void setName(String name) { this.name = name; }
-		public String getKey() { return key; }
-		public void setKey(String key) { this.key = key; }
-		public String getNickname() { return nickname; }
-		public void setNickname(String nickname) { this.nickname = nickname;}
-		public String getSex() { return sex; }
-		public void setSex(String sex) { this.sex = sex; }
-		public String getProfile() { return profile; }
-		public void setProfile(String profile) { this.profile = profile; }
-		public Integer getRegionNo() { return regionNo; }
-		public void setRegionNo(Integer regionNo) { this.regionNo = regionNo; }
-		public String getIntroduction() { return introduction; }
-		public void setIntroduction(String introduction) { this.introduction = introduction; }
-		public Date getRegdate() { return regdate; }
-		public void setRegdate(Date regdate) { this.regdate = regdate; }
-		@Override
-		public String toString() {
-			return "User [no=" + no + ", name=" + name + ", key=" + key + ", nickname=" + nickname + ", sex=" + sex
-					+ ", profile=" + profile + ", regionNo=" + regionNo + ", introduction=" + introduction
-					+ ", regdate=" + regdate + "]";
-		}
-	    
-} //User
+
+	private int no;
+	private String name;
+	private String key;
+	private String nickname;
+	private String sex;
+	private String profile;
+	private Integer regionNo;
+	private String introduction;
+	private Date regdate;
+	
+	public int getNo() { return no; }
+	public String getName() { return name; }
+	public String getKey() { return key; }
+	public String getNickname() { return nickname; }
+	public String getSex() { return sex; }
+	public String getProfile() { return profile; }
+	public Integer getRegionNo() { return regionNo; }
+	public String getIntroduction() { return introduction; }
+	public Date getRegdate() { return regdate; }
+	public void setNo(int no) { this.no = no; }
+	public void setName(String name) { this.name = name; }
+	public void setKey(String key) { this.key = key; }
+	public void setNickname(String nickname) { this.nickname = nickname; }
+	public void setSex(String sex) { this.sex = sex; }
+	public void setProfile(String profile) { this.profile = profile; }
+	public void setRegionNo(Integer regionNo) { this.regionNo = regionNo; }
+	public void setIntroduction(String introduction) { this.introduction = introduction; }
+	public void setRegdate(Date regdate) { this.regdate = regdate; }
+	
+	@Override
+	public String toString() {
+		return "User [no=" + no + ", name=" + name + ", key=" + key + ", nickname=" + nickname + ", sex=" + sex
+				+ ", profile=" + profile + ", regionNo=" + regionNo + ", introduction=" + introduction + ", regdate="
+				+ regdate + "]";
+	} //toString();
+	
+} //class User;
