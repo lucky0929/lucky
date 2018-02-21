28091c014d0bf5e613f7dd2453a1900235a138e7
haseo <haseo@192.168.219.137>
2018-02-21 21:51:37 +0900

    asdf

--- DatePlanner/src/org/dateplanner/vo/Post.java
+++ DatePlanner/src/org/dateplanner/vo/Post.java
@@ -1,28 +1,39 @@
 package org.dateplanner.vo;
 
+import java.sql.Timestamp;
+
 public class Post {
 	
 	private int no;
-	private String main_image;
+	private String mainImage;
 	private String title;
 	private String content;
-	private String packageable;
+	private int userNo;
+	private int regionNo;
+	private Boolean packageable;
+	private Timestamp regdate;
 	
 	public int getNo() { return no; }
-	public String getMain_image() { return main_image; }
+	public String getMainImage() { return mainImage; }
 	public String getTitle() { return title; }
 	public String getContent() { return content; }
-	public String getPackageable() { return packageable; }
+	public int getUserNo() { return userNo; }
+	public int getRegionNo() { return regionNo; }
+	public Boolean getPackageable() { return packageable; }
+	public Timestamp getRegdate() { return regdate; }
 	public void setNo(int no) { this.no = no; }
-	public void setMain_image(String main_image) { this.main_image = main_image; }
+	public void setMainImage(String mainImage) { this.mainImage = mainImage; }
 	public void setTitle(String title) { this.title = title; }
 	public void setContent(String content) { this.content = content; }
-	public void setPackageable(String packageable) { this.packageable = packageable; }
+	public void setUserNo(int userNo) { this.userNo = userNo; }
+	public void setRegionNo(int regionNo) { this.regionNo = regionNo; }
+	public void setPackageable(Boolean packageable) { this.packageable = packageable; }
+	public void setRegdate(Timestamp regdate) { this.regdate = regdate; }
 	
 	@Override
 	public String toString() {
-		return "Post [no=" + no + ", main_image=" + main_image + ", title=" + title + ", content=" + content
-				+ ", packageable=" + packageable + "]";
+		return "Post [no=" + no + ", mainImage=" + mainImage + ", title=" + title + ", content=" + content + ", userNo="
+				+ userNo + ", regionNo=" + regionNo + ", packageable=" + packageable + ", regdate=" + regdate + "]";
 	} //toString();
 	
 } //class Post;
