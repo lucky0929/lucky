283e999916b0da8a1714db48edbcdc775f11b2fa
haseo <haseo@192.168.219.137>
2018-03-02 19:26:22 +0900

    asdf

--- DatePlanner/src/org/dateplanner/controller/MainController.java
+++ DatePlanner/src/org/dateplanner/controller/MainController.java
@@ -1,14 +1,49 @@
 package org.dateplanner.controller;
 
+import java.io.IOException;
+
 import javax.servlet.http.HttpServletRequest;
 
+import org.dateplanner.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Controller
 public class MainController {
 	
-	@RequestMapping(path= { "/", "main", "index" })
-	public String main(HttpServletRequest request) { return "main"; }
+	@RequestMapping(path = { "/", "main", "index" })
+	public ModelAndView main(HttpServletRequest request) {
+		
+		ModelAndView model = new ModelAndView("main");
+		
+		model.addObject("postList", boardService.selectRegdateDesc(0));
+		
+		return model;
+		
+	} //main();
+	
+	@Autowired
+	private BoardService boardService;
+	
+	@RequestMapping(path = "test", params = "no")
+	public ResponseEntity<String> test(int no) throws IOException {
+		
+		Object obj;
+		
+		obj = boardService.selectRegdateDesc(no);
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Content-type", MediaType.APPLICATION_JSON_UTF8_VALUE);
+		return new ResponseEntity<String>(new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(obj), headers, HttpStatus.OK);
+		
+	} //test();
 	
 } //class MainController;
--- DatePlanner/src/org/dateplanner/vo/Post.java
+++ DatePlanner/src/org/dateplanner/vo/Post.java
@@ -12,7 +12,8 @@ public class Post {
 	private int regionNo;
 	private Boolean packageable;
 	private Timestamp regdate;
-	private Double lat, lng;
+	private Double lat;
+	private Double lng;
 	
 	public int getNo() { return no; }
 	public String getImage() { return image; }
@@ -22,9 +23,8 @@ public class Post {
 	public int getRegionNo() { return regionNo; }
 	public Boolean getPackageable() { return packageable; }
 	public Timestamp getRegdate() { return regdate; }
-	public double getLat() { return lat; }
-	public double getLng() { return lng; }
-	
+	public Double getLat() { return lat; }
+	public Double getLng() { return lng; }
 	public void setNo(int no) { this.no = no; }
 	public void setImage(String image) { this.image = image; }
 	public void setTitle(String title) { this.title = title; }
@@ -33,8 +33,8 @@ public class Post {
 	public void setRegionNo(int regionNo) { this.regionNo = regionNo; }
 	public void setPackageable(Boolean packageable) { this.packageable = packageable; }
 	public void setRegdate(Timestamp regdate) { this.regdate = regdate; }
-	public void setLat(double lat) { this.lat = lat; }
-	public void setLng(double lng) { this.lng = lng; }
+	public void setLat(Double lat) { this.lat = lat; }
+	public void setLng(Double lng) { this.lng = lng; }
 	
 	@Override
 	public String toString() {
