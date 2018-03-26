74ef2fd7341d4511e4dab4cbd42812bdbe25225c
dbtjsdlf222 <34783191+dbtjsdlf222@users.noreply.github.com>
2018-03-26 21:16:05 +0900

    Signed-off-by: dbtjsdlf222 <34783191+dbtjsdlf222@users.noreply.github.com>

--- DatePlanner/src/org/dateplanner/controller/CommentController.java
+++ DatePlanner/src/org/dateplanner/controller/CommentController.java
@@ -39,7 +39,7 @@ public class CommentController {
 	}
 	
 	@RequestMapping("commentUpdate")
-	public String commentUpdate(int boardNo, int orderNo, int no,String content, HttpServletRequest req) {
+	public String commentUpdate(int boardNo, int orderNo, int no,String content, HttpServletRequest req) {	
 		commentService.updateCommtent(new Comment(no, orderNo, content));
 		return "redirect:view/"+boardNo;
 	}
