619f497696b2c0573fee17328570b59d143d2c2a
선일 <@Sunil>
2018-03-28 12:35:15 +0900

    Signed-off-by: 선일 <선일@Sunil>

--- DatePlanner/src/org/dateplanner/controller/ErrorController.java
+++ DatePlanner/src/org/dateplanner/controller/ErrorController.java
@@ -7,18 +7,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller @RequestMapping("error")
 public class ErrorController {
-	
 	@RequestMapping("404")
 	 public String handle404(HttpServletResponse res , Exception e) {
 		 res.setStatus(HttpServletResponse.SC_OK); //Response 상태를 정상으로 만듦
-		 System.out.println("404발생");
-		 System.out.println(e.getMessage());
 		 return "redirect:/";
 	 } //404
 	
-	/*@RequestMapping("500")
-	 public String handle500(HttpServletResponse res , Model model) {
+	@RequestMapping("500")
+	 public String handle500(HttpServletResponse res) {
 		 res.setStatus(HttpServletResponse.SC_OK); //Response 상태를 정상으로 만듦
 		 return "redirect:/";
-	 } //500 */
+	 } //500 
 } //class
